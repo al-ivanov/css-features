c571ee5d5f4eb04017605fa73ee371cff90e615c
Alexandr <saanyyaaa@gmail.com>
2019-03-07 14:30:46 +0300

    Добавление меню

--- burger_menu/script.js
+++ burger_menu/script.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menuBtn = document.querySelector('.menu-btn');
+    const menuNav = document.querySelector('.menu-nav');
 
     menuBtn.addEventListener('click', function(e) {
         e.preventDefault();
         this.classList.toggle('menu-btn_active');
+        menuNav.classList.toggle('menu-nav_active');
     });
 });
